3230cba2f5b19b51260e1282f8a9accdea9aa981
dimamuha637296 <dima.muha637296@gmail.com>
2021-11-25 02:55:43 +0300

    sl-main

--- local/src_v6/components/sliders/template/sl-main/script.js
+++ local/src_v6/components/sliders/template/sl-main/script.js
@@ -1,6 +1,28 @@
 !function () {
     'use strict';
 
+    /* eslint-disable-next-line no-unused-vars */
+    function slideCounter(sliderBlock) {
+        var slider = sliderBlock.find('.slider');
+        var currentSlideWrap = sliderBlock.find('.current-slide');
+        /* eslint-disable-next-line no-unused-vars */
+        slider.on('init', function (e, a) {
+            var num = a.currentSlide + 1;
+            if (num < 10) {
+                num = '0' + num;
+            }
+            currentSlideWrap.text(num);
+        });
+        /* eslint-disable-next-line no-unused-vars */
+        slider.on('afterChange', function (e, a) {
+            var num = a.currentSlide + 1;
+            if (num < 10) {
+                num = '0' + num;
+            }
+            currentSlideWrap.text(num);
+        });
+    }
+
     //https://github.com/kenwheeler/slick/
     function initSlider() {
         var block = $('.sl-main');
@@ -17,11 +39,12 @@
             var next = self.find('.next');
             fullWrap.init(self);
             sliderWrap.removeClass('inited-not');
+            slideCounter(block);
             slider.slick({
                 slidesToShow: 1,
                 slidesToScroll: 1,
                 adaptiveHeight: false, // true - height: auto
-                autoplay: true,
+                autoplay: false,
                 autoplaySpeed: 7000,
                 speed: 500,
                 arrows: slidesLength > 1,
@@ -30,14 +53,14 @@
                 dotsClass: 'list-reset', // pager class
                 nextArrow: next,
                 prevArrow: prev,
-                responsive: [
-                    {
-                        breakpoint: ws.const.screen.screen_xs_max,
-                        settings: {
-                            arrows: false
-                        }
-                    }
-                ]
+                // responsive: [
+                //     {
+                //         breakpoint: ws.const.screen.screen_xs_max,
+                //         settings: {
+                //             arrows: false
+                //         }
+                //     }
+                // ]
             });
             sliderWrap.addClass('inited');
         });
