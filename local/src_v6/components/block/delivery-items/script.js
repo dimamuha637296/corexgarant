7bfce1456b6c5d8ca178d9e1d9458f43b25e83cd
dimamuha637296 <dima.muha637296@gmail.com>
2021-12-17 03:42:50 +0300

    calculator-2 page

---
+++ local/src_v6/components/block/delivery-items/script.js
@@ -0,0 +1,25 @@
+!function () {
+    'use strict';
+
+    function toFavorites() {
+        var block = $('.delivery-items');
+        if (block.length) {
+            var btn = block.find('.delivery-items__btn');
+            btn.each(function () {
+                var self = $(this);
+
+                self.off('click.block').on('click.block', function(e) {
+                    e.preventDefault();
+                    $(this).closest('.delivery-items').find('.delivery-items__btn').each(function () {
+                        $(this).removeClass('active');
+                    });
+                    $(this).addClass('active');
+                });
+            });
+        }
+    }
+
+    $(function () {
+        toFavorites();
+    });
+}();
